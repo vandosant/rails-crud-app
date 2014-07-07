4d7ebfb43d7025acb6b048af18a66d74869fbd3b
vandosant <sjskender@gmail.com>
2014-07-07 08:14:43 -0600

    Only wines with ratings 1 to 100 are valid

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,3 +1,4 @@
 class Wine < ActiveRecord::Base
   validates :name, :style, :presence => true
+  validates :rating, :inclusion => {:in => 0..100}
 end
--- spec/models/wine_spec.rb
+++ spec/models/wine_spec.rb
@@ -30,4 +30,32 @@ describe Wine do
 
     expect(wine.valid?).to eq false
   end
+
+  it 'accepts wines with a rating between 0 and 100' do
+    wine = Wine.new
+    wine.name = 'Poison Okra'
+    wine.style = 'Country'
+    wine.color = 'Green'
+    wine.rating = 0
+
+    expect(wine).to be_valid
+
+    wine = Wine.new
+    wine.name = 'Poison Okra'
+    wine.style = 'Country'
+    wine.color = 'Green'
+    wine.rating = 100
+
+    expect(wine).to be_valid
+  end
+
+  it 'does not accept wines with a rating over 100' do
+    wine = Wine.new
+    wine.name = 'Poison Okra'
+    wine.style = 'Country'
+    wine.color = 'Green'
+    wine.rating = 101
+
+    expect(wine).to_not be_valid
+  end
 end
